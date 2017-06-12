bec289a3cc4960779a86b40636918c73e67b6723
Sergio Alexander <xergioalex@gmail.com>
2017-06-12 11:48:11 -0500

    Highlight js added

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,13 +74,13 @@
         });
 
         // Magnific popup script
-        var blogImages = $('.content-magnific-popup img');
+        var blogImages = $('.main-post-content img');
         for (var i = 0; i < blogImages.length; i++) {
             blogImages[i].setAttribute('href', blogImages[i].src);
         }
 
         // Magnific popup init
-        $('.content-magnific-popup img').magnificPopup({
+        $('.main-post-content img').magnificPopup({
             type: 'image',
             // other options
             gallery:{
@@ -88,6 +88,15 @@
             }
         });
 
+        // Add target=_blank to links
+        var linkTags = $('.main-post-content a');
+        for (var i = 0; i < linkTags.length; i++) {
+            linkTags[i].setAttribute('target', '_blank');
+        }
+        // HighlightBlock
+        $('.main-post-content pre code').each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
     });
 
     // Arctic Scroll by Paul Adam Davis
